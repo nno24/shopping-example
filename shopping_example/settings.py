1b277b198308daa6f587160aafbf9ae34314c929
nno24 <nicolai.norseng@gmail.com>
2022-02-10 08:50:57 +0000

    added home app

--- shopping_example/settings.py
+++ shopping_example/settings.py
@@ -43,6 +43,8 @@ INSTALLED_APPS = [
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
+    'home',
+
 ]
 
 MIDDLEWARE = [
@@ -60,7 +62,7 @@ ROOT_URLCONF = 'shopping_example.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates', os.path.join(BASE_DIR, 'templates', 'allauth'))],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
