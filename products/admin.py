3ec1d42d96afc64812b5883cfb05b5afede22556
nno24 <nicolai.norseng@gmail.com>
2022-02-21 12:58:51 +0000

    Fixed spelling of Categorys in admin

--- products/admin.py
+++ products/admin.py
@@ -3,5 +3,23 @@ from .models import Products, Category
 
 # Register your models here.
 
-admin.site.register(Products)
-admin.site.register(Category)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'sku',
+        'name',
+        'category',
+        'price',
+        'rating',
+        'image',
+    )
+
+    ordering = ('sku',)
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+admin.site.register(Products, ProductAdmin)
+admin.site.register(Category, CategoryAdmin)
