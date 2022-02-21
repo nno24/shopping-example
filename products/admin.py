891774fcf9d5260cd8b1e60a868d439c331ffc1a
nno24 <nicolai.norseng@gmail.com>
2022-02-21 13:48:53 +0000

    had to reload fixures with the loaddata command - and renamed Products to Product in the model

--- products/admin.py
+++ products/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Products, Category
+from .models import Product, Category
 
 # Register your models here.
 
@@ -21,5 +21,5 @@ class CategoryAdmin(admin.ModelAdmin):
         'name',
     )
 
-admin.site.register(Products, ProductAdmin)
+admin.site.register(Product, ProductAdmin)
 admin.site.register(Category, CategoryAdmin)
