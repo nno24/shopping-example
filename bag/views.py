1c75462c086c9abab4db0a1fd7e9a4845a381ca6
nno24 <nicolai.norseng@gmail.com>
2022-02-28 15:08:13 +0000

    added add_to_bag func

--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 
@@ -6,3 +6,21 @@ def view_bag(request):
     """ A view to return shopping bag """
 
     return render(request, 'bag/bag.html')
+
+
+def add_to_bag(request, item_id):
+    """ A view to render add to bag """
+    
+    quantity = int(request.POST.get('quantity'))
+    redirect_url = request.POST.get('redirect_url')
+    bag = request.session.get('bag', {})
+    
+    # In the following way, we can save what's in the basket..and the user can keep browse!
+    if item_id in list(bag.keys()):
+        bag[item_id] += quantity
+    else:
+        bag[item_id] = quantity
+    
+    request.session['bag'] = bag
+    print(request.session['bag'])
+    return redirect(redirect_url)
